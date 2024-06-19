ad9fb3151984653c1983d5af1e8b1bbc78490c3c
Arrow A <141832297+Arrowaaa@users.noreply.github.com>
2024-06-19 14:31:40 -0300

    P. Aula da UC 11 do dia 12/06/2024 melhorias 19/06

--- Primeira-Aula-da-UC11/js/script.js
+++ Primeira-Aula-da-UC11/js/script.js
@@ -1,68 +1,8 @@
-// let carrinho = [];
-// let total = 0;
 
-// function addCarrinho(produto, valor) {
-//     let fim = false;
-//     for (let i = 0; i < carrinho.length; i++) {
-//         if (carrinho[i].produto === produto) {
-//             carrinho[i].qtd++;
-//             fim = true;
-//             break;
-//         }
-//     }
-//     if (!fim) {
-//         carrinho.push({ produto, valor, imagem, qtd: 1 }); // a função push é para puxar as informação do carrinho
-//     }
-//     atualizaCarrinho();
-// }
-
-// function removeItemCarrinho(index) { //Função para remover itens de carrinho.
-
-//     carrinho.splice(index, 1);
-//     atualizaCarrinho();
-// }
-
-// // function removeItemCarrinho(index) { Outra forma para remover itens de carrinhos de compras.
-// //     total -= carrinho[index].valor * carrinho[index].qtd; 
-// //     carrinho.splice(index, 1);
-// //     atualizaCarrinho();
-// // }
-
-// function atualizaCarrinho() {
-//     const cartItens = document.getElementById('carrinho-itens');
-//     cartItens.innerHTML = '';
-//     total = 0;
-//     carrinho.forEach((item, index) => {
-//         total += item.valor * item.qtd;
-//         cartItens.innerHTML += `
-//             <div class="carrinho-item">
-//                 <img src="${item.imagem}" alt="${item.produto}" class="carrinho-img">
-//                 <span>${item.produto} (X ${item.qtd})</span>
-//                 <span>R$ ${item.valor * item.qtd}</span>
-//                 <button onClick="removeItemCarrinho(${index})">Remover</button>
-//             </div>
-//         `;
-//     });
-//     document.getElementById('carrinho-total').innerText = total;
-// }
-
-// function fecharPedido(){ // função para fechar a comprar e emitir um alerta para se a compra for bem sucedida ou não.
-//     if(carrinho.length == 0){
-//         alert(`Seu Carrinho Está Vazio`);
-//         return;
-//     }
-//     alert(`Compra Finalizada! Total: R$ ${total}`);
-//     carrinho = [];
-//     atualizaCarrinho;
-
-// }
-
-
-
-let carrinho = [];
+let carrinho = []; // declara o arrey
 let total = 0;
 
-function addCarrinho(produto, valor, imagem) {
+function addCarrinho(produto, valor, imagem) { // função para adicionar os produtos no carrinho
     let fim = false;
     for (let i = 0; i < carrinho.length; i++) {
         if (carrinho[i].produto === produto) {
@@ -72,15 +12,21 @@ function addCarrinho(produto, valor, imagem) {
         }
     }
     if (!fim) {
-        carrinho.push({ produto, valor, imagem, qtd: 1 });
+        carrinho.push({ produto, valor, imagem, qtd: 1 });  // a função push é para puxar as informação do carrinho
     }
     atualizaCarrinho();
 }
 
-function removeItemCarrinho(index) {
+function removeItemCarrinho(index) { //Função para remover itens de carrinho.
     carrinho.splice(index, 1);
     atualizaCarrinho();
 }
+
+// // function removeItemCarrinho(index) { Outra forma para remover itens de carrinhos de compras.
+// //     total -= carrinho[index].valor * carrinho[index].qtd; 
+// //     carrinho.splice(index, 1);
+// //     atualizaCarrinho();
+// // }
 
 function atualizaCarrinho() {
     const cartItens = document.getElementById('carrinho-itens');
@@ -91,16 +37,20 @@ function atualizaCarrinho() {
         cartItens.innerHTML += `
             <div class="carrinho-item">
                 <img src="${item.imagem}" alt="${item.produto}" class="carrinho-img">
+                <br>
                 <span>${item.produto} (X ${item.qtd})</span>
                 <span>R$ ${item.valor * item.qtd}</span>
-                <button onClick="removeItemCarrinho(${index})">Remover</button>
+                
+                <button class="btn-remover" onClick="removeItemCarrinho(${index})">Remover</button>
             </div>
-        `;
+            <br>
+        `; 
+        
     });
     document.getElementById('carrinho-total').innerText = total;
 }
 
-function fecharPedido() {
+function fecharPedido() { // função para fechar a comprar e emitir um alerta para se a compra for bem sucedida ou não.
     if (carrinho.length == 0) {
         alert(`Seu Carrinho Está Vazio`);
         return;
